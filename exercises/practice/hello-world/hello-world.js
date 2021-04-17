78cf0c5350df30f814e965fab1eac77e3102a99d
stacyburris <stacy1burris@gmail.com>
2021-04-17 09:51:40 -0700

    begin learning through exercism site

--- exercises/practice/hello-world/hello-world.js
+++ exercises/practice/hello-world/hello-world.js
@@ -1,8 +1,7 @@
-//
-// This is only a SKELETON file for the 'Hello World' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+'use strict';
+
+
 
 export const hello = () => {
-  throw new Error('Remove this statement and implement this function');
+ return "Hello, World!"
 };
